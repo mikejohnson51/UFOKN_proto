4b2b3230b5d65e033e59c3435cb65a0c7868f9b5
David Blodgett <dblodgett@usgs.gov>
2019-10-31 11:34:20 -0500

    copy pasta bug

--- runner.R
+++ runner.R
@@ -18,10 +18,10 @@ plan <- drake_plan(out_prj = 4326, # wgs84 lat/lon
                    close_gages = find_gage_points(roads, gages, ann_prj, dist),
                    roads_base_uri = "https://ufokn.demo/roads/",
                    gages_base_uri = "https://ufokn.demo/gages/",
-                   roads_rdf = target(mint_feature(subject = paste0(roads_base_uri, roads$LINEARID), 
+                   roads_rdf = mint_feature(subject = paste0(roads_base_uri, roads$LINEARID), 
                                             type = "https://ufokn.demo/type/road/",
                                             label = roads$Name, 
-                                            rdf = rdf())),
+                                            rdf = rdf()),
                    roads_rdf2 = create_seealso(subject = paste0(roads_base_uri, roads$LINEARID), 
                                                seealso = paste0(roads_base_uri, "get_data/", roads$LINEARID),
                                                format = "application/vnd.geo+json", 
@@ -44,14 +44,17 @@ plan <- drake_plan(out_prj = 4326, # wgs84 lat/lon
                                              rdf = rdf_parse(gages_rdf2, format = "turtle")),
                    gages_roads_rdf = create_association(subject = paste0(roads_base_uri, close_gages$road_LINEARID), 
                                                         predicate = "https://ufokn.demo/association/nearbyGage", 
-                                                        object = paste0(gages_base_uri, close_gages$road_LINEARID),
+                                                        object = paste0(gages_base_uri, close_gages$gage_Name),
                                                         rdf = rdf()),
                    roads_out_file = "output/roads.rdf",
-                   target(rdf_serialize(rdf_parse(roads_rdf3, format = "turtle"), doc = roads_out_file), file_out(roads_out_file)),
+                   target(rdf_serialize(rdf_parse(roads_rdf3, format = "turtle"), doc = roads_out_file), 
+                          file_out(roads_out_file)),
                    gages_out_file = "output/gages.rdf",
-                   target(rdf_serialize(rdf_parse(gages_rdf3, format = "turtle"), doc = gages_out_file), file_out(gages_out_file)),
+                   target(rdf_serialize(rdf_parse(gages_rdf3, format = "turtle"), doc = gages_out_file), 
+                          file_out(gages_out_file)),
                    gages_roads_out_file = "output/gages_roads.rdf",
-                   target(rdf_serialize(rdf_parse(gages_roads_rdf, format = "turtle"), doc = gages_roads_out_file), file_out(gages_roads_out_file)))
+                   target(rdf_serialize(rdf_parse(gages_roads_rdf, format = "turtle"), doc = gages_roads_out_file), 
+                          file_out(gages_roads_out_file)))
 
 clean()
 
